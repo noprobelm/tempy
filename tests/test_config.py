0ba789a747045b9c9efbcdb109fb5f138ce1b95b
noprobelm <noprobelm@protonmail.com>
2023-06-26 12:19:29 -0400

    Cleared out test_configs (repurposing)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,14 @@
 from tempy import config
-from ward import test
+from ward import test, raises
+from pathlib import Path
+import shutil
+import os
+
+
+@test("Valid options are 'location', 'units', 'api_key'")
+def _():
+    assert config.VALID_OPTIONS == ("location", "units", "api_key")
+
 
 tempyrc_permutations = {
     "empty": ("", "", ""),
@@ -24,3 +33,57 @@ for fname in tempyrc_permutations:
             tempyrc["units"],
             tempyrc["api_key"],
         )
+
+
+@test("config.TempyRC will ignore invalid config options")
+def _():
+    tempyrc = config.TempyRC("sample_tempyrcs/invalid")
+    assert "invalid" not in tempyrc.keys()
+
+
+@test("config.TempyRC will ignore commented lines")
+def _():
+    tempyrc = config.TempyRC("sample_tempyrcs/commented")
+    assert tempyrc == {
+        "location": "",
+        "units": "imperial",
+        "api_key": "jlskdjfaklejaeglkw",
+    }
+
+
+@test(
+    "config.TempyRC will convert arg 'path' to a pathlib.Path object at the specified path"
+)
+def _():
+    tempyrc = config.TempyRC(f"sample_tempyrcs/location")
+    assert isinstance(tempyrc.path, Path)
+    assert tempyrc.path == Path("sample_tempyrcs/location")
+
+
+@test(
+    "config.TempyRC will create a new path with a tempyrc skel if the arg 'path' does not exist"
+)
+def _():
+    parent_path = "sample_tempyrcs/new_path"
+    tempyrc_path = os.path.join(parent_path, "tempyrc")
+    assert os.path.isdir(parent_path) == False
+
+    tempyrc = config.TempyRC(os.path.join(parent_path, "tempyrc"))
+    assert os.path.isdir(parent_path) == True
+    assert os.path.isfile(tempyrc_path) == True
+
+    shutil.rmtree(parent_path, ignore_errors=True)
+
+
+@test(
+    "config.TempyRC will not create a new path with a tempyrc skel if the parent path's parent does not exist (to prevent recursive mkdir)"
+)
+def _():
+    parent_path = "sample_tempyrcs/new_path_1/new_path_2"
+    tempyrc_path = os.path.join(parent_path, "tempyrc")
+    assert os.path.isdir(parent_path) == False
+
+    with raises(SystemExit) as err:
+        tempyrc = config.TempyRC(os.path.join(parent_path, "tempyrc"))
+
+    assert os.path.isdir(parent_path) == False
