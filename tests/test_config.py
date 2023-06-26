a4202decc7ba674f75ffd89516e054331267a391
noprobelm <noprobelm@protonmail.com>
2023-06-26 13:11:03 -0400

    Added sample tempyrc and remaining tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,85 +5,55 @@ import shutil
 import os
 
 
-@test("Valid options are 'location', 'units', 'api_key'")
+@test("Config will instantiate with no arguments")
 def _():
-    assert config.VALID_OPTIONS == ("location", "units", "api_key")
+    tempy_config = config.Config()
+    assert isinstance(tempy_config, config.Config)
 
 
-tempyrc_permutations = {
-    "empty": ("", "", ""),
-    "location": ("nyc", "", ""),
-    "location_units": ("nyc", "imperial", ""),
-    "location_units_api": ("nyc", "imperial", "jlskdjfaklejaeglkw"),
-    "units_api": ("", "imperial", "jlskdjfaklejaeglkw"),
-    "api": ("", "", "jlskdjfaklejaeglkw"),
-    "units": ("", "imperial", ""),
-    "location_api": ("nyc", "", "jlskdjfaklejaeglkw"),
-}
-
-for fname in tempyrc_permutations:
-
-    @test(
-        f"config.TempyRC will parse location={tempyrc_permutations[fname][0]}, units={tempyrc_permutations[fname][1]}, api_key={tempyrc_permutations[fname][2]}"
-    )
-    def _():
-        tempyrc = config.TempyRC(f"sample_tempyrcs/{fname}")
-        assert tempyrc_permutations[fname] == (
-            tempyrc["location"],
-            tempyrc["units"],
-            tempyrc["api_key"],
-        )
-
-
-@test("config.TempyRC will ignore invalid config options")
+@test("A valid config.TempyRC was parsed")
 def _():
-    tempyrc = config.TempyRC("sample_tempyrcs/invalid")
-    assert "invalid" not in tempyrc.keys()
+    tempy_config = config.Config()
+    assert all(key in tempy_config._tempyrc for key in config.VALID_OPTIONS)
 
 
-@test("config.TempyRC will ignore commented lines")
+@test("A valid config.Args was parsed")
 def _():
-    tempyrc = config.TempyRC("sample_tempyrcs/commented")
-    assert tempyrc == {
-        "location": "",
-        "units": "imperial",
-        "api_key": "jlskdjfaklejaeglkw",
-    }
+    tempy_config = config.Config()
+    assert all(key in tempy_config._args for key in config.VALID_OPTIONS)
 
 
-@test(
-    "config.TempyRC will convert arg 'path' to a pathlib.Path object at the specified path"
-)
+@test("Command line arguments take priority over tempyrc (if it exists in tempyrc)")
 def _():
-    tempyrc = config.TempyRC(f"sample_tempyrcs/location")
-    assert isinstance(tempyrc.path, Path)
-    assert tempyrc.path == Path("sample_tempyrcs/location")
+    tempyrc_path = "sample_tempyrcs/location_units_api"
+    tempyrc = config.TempyRC(tempyrc_path)
+    unparsed = ["beijing", "--units", "metric"]
+
+    tempy_config = config.Config(tempyrc_path, unparsed)
+
+    # Args take priority
+    assert tempy_config["location"] == "beijing"
+    # Args take priority
+    assert tempy_config["units"] == "metric"
+
+    # Missing args for api_key, so tempyrc takes priority
+    assert tempy_config["api_key"] == tempyrc["api_key"]
 
 
-@test(
-    "config.TempyRC will create a new path with a tempyrc skel if the arg 'path' does not exist"
-)
+@test("Tempy terminates if no location is found in tempyrc or command line args")
 def _():
-    parent_path = "sample_tempyrcs/new_path"
-    tempyrc_path = os.path.join(parent_path, "tempyrc")
-    assert os.path.isdir(parent_path) == False
+    tempyrc_path = "sample_tempyrcs/empty"
+    unparsed = []
 
-    tempyrc = config.TempyRC(os.path.join(parent_path, "tempyrc"))
-    assert os.path.isdir(parent_path) == True
-    assert os.path.isfile(tempyrc_path) == True
-
-    shutil.rmtree(parent_path, ignore_errors=True)
+    with raises(SystemExit):
+        tempy_config = config.Config(tempyrc_path, unparsed)
 
 
-@test(
-    "config.TempyRC will not create a new path with a tempyrc skel if the parent path's parent does not exist (to prevent recursive mkdir)"
-)
+@test("Config will set 'units' default to 'imperial' if no units are specified")
 def _():
-    parent_path = "sample_tempyrcs/new_path_1/new_path_2"
-    tempyrc_path = os.path.join(parent_path, "tempyrc")
-    assert os.path.isdir(parent_path) == False
+    tempyrc_path = "sample_tempyrcs/location"
+    unparsed = []
 
-    with raises(SystemExit) as err:
-        tempyrc = config.TempyRC(os.path.join(parent_path, "tempyrc"))
+    tempy_config = config.Config(tempyrc_path, unparsed)
 
-    assert os.path.isdir(parent_path) == False
+    assert tempy_config["units"] == "imperial"
