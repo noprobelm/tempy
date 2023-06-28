cccc3826a61ce902500ff63a85ecb078b34fd332
noprobelm <noprobelm@protonmail.com>
2023-06-28 10:59:30 -0400

    Added tests for parser

---
+++ tests/test_parser.py
@@ -0,0 +1,29 @@
+from ward import test
+from tempy.parser import parse_rc
+
+tempyrc_permutations = {
+    "empty": {},
+    "location": {"location": "nyc"},
+    "location_units": {"location": "nyc", "units": "imperial"},
+    "location_units_api": {
+        "location": "nyc",
+        "units": "imperial",
+        "api_key": "jlskdjfaklejaeglkw",
+    },
+    "units_api": {"units": "imperial", "api_key": "jlskdjfaklejaeglkw"},
+    "api": {"location": "", "units": "", "api_key": "jlskdjfaklejaeglkw"},
+    "units": {"location": "", "units": "imperial", "api_key": ""},
+    "location_api": {
+        "location": "nyc",
+        "units": "",
+        "api_key": "jlskdjfaklejaeglkw",
+    },
+}
+
+for fname in tempyrc_permutations:
+
+    @test("All possible permutation of tempyrc are parsed")
+    def _():
+        with open(f"sample_tempyrcs/{fname}", "r") as fp:
+            parsed = parse_rc(fp)
+        assert parsed == tempyrc_permutations[fname]
