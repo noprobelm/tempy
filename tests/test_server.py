0b7423e89efa0d73c7a4f57b35dc6ed5c33d7e2b
noprobelm <noprobelm@protonmail.com>
2023-06-30 14:14:20 -0400

    Added test for weatherapi.com requests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,8 +1,21 @@
 from ward import test
 import socket
+import os
+from tempy import config
 from tempy import data
 
 
 @test("Proxy server domain name resolves to 173.255.235.176")
 def _():
     assert "173.255.235.176" == socket.gethostbyname(data.Data._proxy_domain)
+
+
+@test("Request weatherapi returns valid data")
+def _():
+    tempyrc = config.TempyRC(
+        os.path.join(os.path.expanduser("~"), ".config/tempyrc_test")
+    )
+    weather_data = data.Data(location=tempyrc["location"], api_key=tempyrc["api_key"])
+
+    for key in ["localdata", "weather", "forecast"]:
+        assert key in weather_data.keys()
