6a2147e5f569bde9e0433e2943f31713bcd65a03
noprobelm <noprobelm@protonmail.com>
2023-06-30 09:18:44 -0400

    Reworked config classmethod to be from_default
    Decided it would be better to update Config.from_cli to
    Config.from_default. Moved tempyrc_path determination logic back into
    the classmethod

--- tempy/__main__.py
+++ tempy/__main__.py
@@ -7,12 +7,7 @@ from .weather import Report
 
 
 def main():
-    if os.name == "nt":
-        tempyrc_path = f"{os.path.expanduser('~')}\\AppData\\Roaming\\tempyrc"
-    else:
-        tempyrc_path = f"{os.path.expanduser('~')}/.config/tempyrc"
-
-    config = Config.from_unparsed(tempyrc_path=tempyrc_path, unparsed=sys.argv[1:])
+    config = Config.from_default()
     report = Report(config["location"], config["units"], config["api_key"])
     console.print(report)
 
