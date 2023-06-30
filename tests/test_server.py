605751a01f64064621031bb82a460b680ec2de32
noprobelm <noprobelm@protonmail.com>
2023-06-30 14:16:57 -0400

    Added test for request to proxy server

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,10 +4,21 @@ import os
 from tempy import config
 from tempy import data
 
+KEYS = ["localdata", "weather", "forecast"]
+
 
 @test("Proxy server domain name resolves to 173.255.235.176")
 def _():
     assert "173.255.235.176" == socket.gethostbyname(data.Data._proxy_domain)
+
+
+@test("Request to proxy server returns valid data")
+def _():
+    args = config.Args(["nyc"])
+    weather_data = data.Data(args["location"])
+
+    for key in KEYS:
+        assert key in weather_data.keys()
 
 
 @test("Request to weatherapi returns valid data")
@@ -17,5 +28,5 @@ def _():
     )
     weather_data = data.Data(location=tempyrc["location"], api_key=tempyrc["api_key"])
 
-    for key in ["localdata", "weather", "forecast"]:
+    for key in KEYS:
         assert key in weather_data.keys()
