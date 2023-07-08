41738b32c8f83421eca5102981b48b0ca061354d
noprobelm <noprobelm@protonmail.com>
2023-07-08 17:06:36 -0400

    Rewrote Report class
    - Moved Weather and WeatherArt classes to be methods of Report
    - Wrote general improvements for readability / concision

--- tempy/weather.py
+++ tempy/weather.py
@@ -1,9 +1,9 @@
+import os
 from pathlib import Path
-from typing import List, Optional, Union
 
 from rich import box
 from rich.align import Align
-from rich.console import Console, ConsoleOptions, Group, Measurement, RenderResult
+from rich.console import Console, ConsoleOptions, Group, RenderResult
 from rich.panel import Panel
 from rich.rule import Rule
 from rich.table import Column, Table
@@ -13,28 +13,54 @@ from .console import console
 from .data import Data
 from .themes import Default
 
-from dataclasses import dataclass
 
+class Report:
+    def __init__(self, location: str, units: str, api_key: str) -> None:
+        self._data = Data(location, api_key)
 
-@dataclass
-class Weather:
-    title: str
-    weather: dict
+        if units == "imperial":
+            self._imperial = True
+        else:
+            self._imperial = False
 
-    def __rich_console__(
-        self, console: Console, options: ConsoleOptions
-    ) -> RenderResult:
+    def _get_ascii_art(self, condition: str, is_day: bool):
+        assets_path = os.path.join(Path(__file__).parent, "assets")
+        filename = condition.replace(" ", "_").lower()
+        if is_day is True:
+            filename = f"{filename}_day.txt"
+        else:
+            filename = f"{filename}_night.txt"
+
+        asset_path = os.path.join(assets_path, filename)
+
+        with open(asset_path, "r") as f:
+            asset = f.read()
+
+        art = Text.from_ansi(asset)
+        width = console.measure(art).maximum
+
+        label = Text(
+            condition.title().center(width),
+            style=Default.labels,
+        )
+
+        art.append("\n\n")
+        art.append(label)
+
+        return art
+
+    def _get_weather_table(self, table_data: dict, title: str) -> Table:
         labels = Column(
             "labels",
-            width=max([len(label) for label in self.weather.keys()]),
+            width=max([len(label) for label in table_data.keys()]),
             no_wrap=True,
-            style=Default.information,
+            style=Default.labels,
         )
         values = Column(
             "values",
-            width=max([len(str(value)) for value in self.weather.values()]),
+            width=max([len(str(value)) for value in table_data.values()]),
             no_wrap=True,
-            style=Default.information,
+            style=Default.labels,
         )
         table = Table(
             labels,
@@ -46,84 +72,53 @@ class Weather:
             border_style=Default.table_divider,
         )
 
-        table.title = f"{self.title}\n"
-        for label in self.weather:
-            table.add_row(label.title(), self.weather[label])
+        table.title = f"{title}\n"
+        for label in table_data:
+            table.add_row(label.title(), table_data[label])
 
-        yield table
-
-
-class Report:
-    def __init__(self, location: str, units: str, api_key: str) -> None:
-        self.data = Data(location, api_key)
-        self.units = units
-
-    @property
-    def location(self) -> Text:
-        return Text(self.data["localdata"]["location"], style=Default.report_header)
-
-    @property
-    def localtime(self) -> Text:
-        return Text(self.data["localdata"]["localtime"], style=Default.report_header)
-
-    @property
-    def art(self) -> Text:
-        if self.data["weather"]["is_day"]:
-            file_suffix = "day"
-        else:
-            file_suffix = "night"
-        parent = Path(__file__).parent.parent
-        filename = f"{parent}/tempy/assets/{self.data['weather']['condition'].replace(' ', '_')}_{file_suffix}.txt".lower()
-        with open(filename, "r") as f:
-            art = Text.from_ansi(f.read())
-
-        width = console.measure(art).maximum
-        # TODO Consider replacing label with a table/grid footer that has a single empty line above it.
-        label = Text(
-            self.data["weather"]["condition"].title().center(width),
-            style=Default.information,
-        )
-        art = art.append("\n\n")
-        art.append(label)
-        return art
-
-    @property
-    def weather_table(self) -> Weather:
-        if self.units == "imperial":
-            weather_table = Weather(
-                "Current Conditions\n", self.data["weather"]["imperial"]
-            )
-        else:
-            weather_table = Weather(
-                "Current Conditions\n", self.data["weather"]["metric"]
-            )
-
-        return weather_table
-
-    @property
-    def forecast_tables(self) -> List[Weather]:
-        forecast = self.data["forecast"]
-        if self.units == "imperial":
-            forecast_tables = [Weather("Today's Forecast\n", forecast[0]["imperial"])]
-            for data in forecast[1:]:
-                forecast_tables.append(Weather(f"{data['date']}\n", data["imperial"]))
-        else:
-            forecast_tables = [Weather("Today's Forecast\n", forecast[0]["metric"])]
-            for data in forecast[1:]:
-                forecast_tables.append(Weather(f"{data['date']}\n", data["metric"]))
-
-        return forecast_tables
+        return table
 
     def __rich_console__(
         self, console: Console, options: ConsoleOptions
     ) -> RenderResult:
-        location = self.location
-        localtime = self.localtime
-        art = self.art
-        weather_table = self.weather_table
-        forecast_tables = self.forecast_tables
+        location = Text(
+            self._data["localdata"]["location"], style=Default.report_header
+        )
+        localtime = Text(
+            self._data["localdata"]["localtime"], style=Default.report_header
+        )
+        art = self._get_ascii_art(
+            self._data["weather"]["condition"], self._data["weather"]["is_day"]
+        )
+        if self._imperial == True:
+            weather_table = self._get_weather_table(
+                self._data["weather"]["imperial"], "Current Conditions"
+            )
 
-        today = Table(
+            forecast = self._data["forecast"]
+            forecast_tables = [
+                self._get_weather_table(forecast[0]["imperial"], "Today's Forecast")
+            ]
+            for day in forecast[1:]:
+                forecast_tables.append(
+                    self._get_weather_table(day["imperial"], f"{day['date']}")
+                )
+
+        else:
+            weather_table = self._get_weather_table(
+                self._data["weather"]["metric"], "Current Conditions"
+            )
+
+            forecast = self._data["forecast"]
+            forecast_tables = [
+                self._get_weather_table(forecast[0]["metric"], "Today's Forecast")
+            ]
+            for day in forecast[1:]:
+                forecast_tables.append(
+                    self._get_weather_table(day["metric"], f"{day['date']}")
+                )
+
+        report_upper = Table(
             Column("Art"),
             Column("Current Conditions"),
             Column("Today's Forecast"),
@@ -131,23 +126,25 @@ class Report:
             box=None,
             show_header=False,
         )
-        today.add_row(Align(art, vertical="middle"), weather_table, forecast_tables[0])
-        # BUG: There might be a bug in the way Panel.fit() calculates the size it needs to be for grids that have padding, hence the explicit definition below. Reminder to look into it.
-        today.width = (console.measure(today).maximum) + sum(
-            [today.padding[1], today.padding[3]]
+        report_upper.add_row(
+            Align(art, vertical="middle"), weather_table, forecast_tables[0]
         )
-        today = Panel.fit(
+        # BUG: There might be a bug in the way Panel.fit() calculates the size it needs to be for grids that have padding, hence the explicit definition below. Reminder to look into it.
+        report_upper.width = (console.measure(report_upper).maximum) + sum(
+            [report_upper.padding[1], report_upper.padding[3]]
+        )
+        report_upper = Panel.fit(
             Group(
                 Align(location, "center"),
                 Align(localtime, "center"),
                 Rule(style=Default.panel_border_style),
-                today,
+                report_upper,
             ),
             box=box.HEAVY,
             border_style=Default.panel_border_style,
         )
 
-        future = Table(
+        report_lower = Table(
             Column("1", justify="left"),
             Column("2", justify="right"),
             show_edge=False,
@@ -156,22 +153,23 @@ class Report:
             show_header=False,
             padding=0,
         )
-        future.width = console.measure(today).maximum
-        future.add_row(
+
+        report_lower.width = console.measure(report_upper).maximum
+        report_lower.add_row(
             Panel(
                 Align(forecast_tables[1], "center"),
                 box=box.HEAVY,
                 style=Default.panel_border_style,
-                width=(future.width // 2 - 1),
+                width=(report_lower.width // 2 - 1),
             ),
             Panel(
                 Align(forecast_tables[2], "center"),
                 box=box.HEAVY,
                 style=Default.panel_border_style,
-                width=(future.width // 2 + 1)
-                if future.width % 2 == 1
-                else future.width // 2,
+                width=(report_lower.width // 2 + 1)
+                if report_lower.width % 2 == 1
+                else report_lower.width // 2,
             ),
         )
-        yield today
-        yield future
+        yield report_upper
+        yield report_lower
